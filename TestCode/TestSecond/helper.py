179c02539ff1395f1e2b38b6c237db92bdb288b9
Akshata Deo <akshatadeo04@gmail.com>
2022-03-16 15:44:21 -0700

    Added new Test

---
+++ TestCode/TestSecond/helper.py
@@ -0,0 +1,2 @@
+def convertHr(hr):
+    if hr >= 12: return hr - 12
---
+++ TestCode/TestSecond/second.py
@@ -0,0 +1,10 @@
+import sys
+import helper
+
+def time(hr, min, sec):
+    convertHr(hr)
+    print('The time is ', hr, ' ', min, ' ', sec)
+
+
+time(10,20,20)
+time(20,30,40)
